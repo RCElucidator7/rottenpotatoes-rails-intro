22d930132ae1fd828c1f2a8e442550d98248ba1a
EC2 Default User <ec2-user@ip-172-31-0-148.us-east-2.compute.internal>
2019-02-21 13:41:46 +0000

    Added ratings + refresh button

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,4 @@
 class MoviesController < ApplicationController
-  helper_method :hilight
-
 
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
@@ -13,14 +11,18 @@ class MoviesController < ApplicationController
   end
 
   def index
-    #@movies = Movie.all
+    @all_ratings = Movie.ratings
+    @selected_ratings = @all_ratings
+    
     # sort based on params[:sort]
     @sort_column = params[:sort] || 'id'
+    
+    @selected_ratings =  params[:ratings].keys if params[:ratings]
 
     @title_hilite = "hilite" if params[:sort] == 'title'
     @date_hilite = "hilite" if params[:sort] == 'release_date'
 
-    @movies = Movie.order(@sort_column)
+    @movies = Movie.where(rating: @selected_ratings).order(@sort_column)
   end
 
   def new
